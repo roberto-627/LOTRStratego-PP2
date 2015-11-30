19de10740f8c68d0a9e2fef9acbd67c06e800f57
Roberto Melara <roberto.melara@unitec.edu>
2015-11-29 21:30:52 -0600

    Version Estable 0.1

---
+++ LOTR-Stratego/src/Fichas/FichaRango3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fichas;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Roberto Melara
+ */
+public class FichaRango3 extends Ficha implements Movable{
+    
+    public FichaRango3(int f, String n){
+        faccion = f;
+        rango = 3;
+        if(f == 1){
+            nombre = n;
+            icono = new ImageIcon("Fichas/Gondor/"+ nombre +" (Custom).png");
+            previa = new ImageIcon("Fichas/Gondor/"+ nombre +".png");
+        }else{
+            nombre = n;
+            icono = new ImageIcon("Fichas/Mordor/"+ nombre +" (Custom).png");
+            previa = new ImageIcon("Fichas/Mordor/"+ nombre +".png");
+        }
+    }
+    @Override
+    public void mover() {}
+}
